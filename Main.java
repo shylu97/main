816e2c6abe41377a0091a2a1fb38d6b5ced66542
shylu97 <134427652+shylu97@users.noreply.github.com>
2023-05-24 00:22:37 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+class basic {
+    public static void main(String[]args){
+        System.out.println("enter the name");
+        Scanner input=new Scanner(System.in);
+        String name=input.next();
+        System.out.println("hello " +name);
+    }
+}
